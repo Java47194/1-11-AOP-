12a8803cba713cd6a0846b6254c266aa4ccf8a59
lee <tlkj1@naver.com>
2023-06-12 06:58:30 +0900

    김영한 mvc 정적

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -8,13 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
     @GetMapping("hello")
-  public String hello(Model model){
-        model.addAttribute("data","hello");
+    public String hello(Model model) {
+        model.addAttribute("data", "hello");
         return "hello";
     }
+
     @GetMapping("hello-mvc")
-    public  String helloMvc(@RequestParam("name" ) String name , Model model){
-        model.addAttribute("name",name);
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
         return "hello-template";
     }
 }
