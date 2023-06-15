01a4ad92f1e1ef36cc5ed7a22c489dda7e094ffa
lee <tlkj1@naver.com>
2023-06-15 17:08:45 +0900

    김영한 spring h2 database 관리

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -11,5 +17,30 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+        //get 방식은 조회 post는 등록
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        System.out.println("form =" +form.getName());
+        Member member=new Member();
+        member.setName(form.getName());
+        System.out.println("member1 =" +member);
+        memberService.join(member);
+        System.out.println("member2 =" +member);
+
+        return "redirect:/";
+
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> list = memberService.findMembers();
+        model.addAttribute("list",list);
+        return "members/memberList";
     }
 }
