df33fdb1f3a8c530fd2a4241902521314692090a
lee <tlkj1@naver.com>
2023-06-16 11:43:55 +0900

    김영한 spring h2 database 관리

--- hello-spring/src/main/java/hello/hellospring/SpingConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpingConfig.java
@@ -1,13 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpingConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpingConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,8 @@ public class SpingConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return  new MemoryMemberRepository();
+        //이게 다형성
+        //return  new MemoryMemberRepository();
+        return  new JdbcMemberRepository(dataSource);
     }
 }
