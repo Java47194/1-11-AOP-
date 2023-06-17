9bf264c36d574eda586e5050fa7034a6a7b6eace
lee <tlkj1@naver.com>
2023-06-17 14:59:15 +0900

    김영한 jpa

--- hello-spring/src/main/java/hello/hellospring/SpingConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpingConfig.java
@@ -1,25 +1,29 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpingConfig {
 
-    private DataSource dataSource;
+    private EntityManager em;
+
+    public SpingConfig(EntityManager em) {
+        this.em = em;
+    }
+
+    /*  private DataSource dataSource;
 
     @Autowired
     public SpingConfig(DataSource dataSource) {
         this.dataSource = dataSource;
-    }
+    }*/
 
     @Bean
     public MemberService memberService() {
@@ -32,6 +36,7 @@ public class SpingConfig {
         //return  new MemoryMemberRepository();
        //return  new JdbcMemberRepository(dataSource);
 
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,6 +39,7 @@ class MemberServiceIntegrationTest {
 */
 
     @Test
+
     void 회원가입() {
         //given 데이트 기반
         Member member=new Member();
